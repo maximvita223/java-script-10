bee545a7269d2187cbc398e3fc33cc6000a7490f
maximvita223 <109244209+maximvita223@users.noreply.github.com>
2024-10-10 22:22:30 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const dataInfo = `[{
+"id": "31p",
+"name": "Kitty",
+"facts": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, debitis.",
+"url": "https://cdn2.thecatapi.com/images/31p.jpg",
+"width": 640,
+"height": 458
+},
+{
+"id": "48e",
+"name": "Shelly",
+"facts": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, debitis.",
+"url": "https://cdn2.thecatapi.com/images/48e.gif",
+"width": 250,
+"height": 188
+},
+{
+"id": "62t",
+"name": "Tommy",
+"facts": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, debitis.",
+"url": "https://cdn2.thecatapi.com/images/62t.jpg",
+"width": 1536,
+"height": 2048
+}]
+`
